d33a3c308ed803acbd8ebd877cf65ce1b9103b21
jigsaw <m@jgs.me>
2022-09-15 15:46:10 +0900

    Add register command

---
+++ src/register.ts
@@ -0,0 +1,21 @@
+import "dotenv";
+
+fetch(
+  `https://discord.com/api/v10/applications/${
+    Deno.env.get("DISCORD_APPLICATION_ID")
+  }/guilds/${Deno.env.get("DISCORD_GUILD_ID")}/commands`,
+  {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bot ${Deno.env.get("DISCORD_TOKEN")}`,
+    },
+    method: "PUT",
+    body: JSON.stringify([{
+      name: "ping1",
+      type: 3,
+    }]),
+  },
+).then(async (res) => {
+  const json = await res.json();
+  console.log(json);
+});
